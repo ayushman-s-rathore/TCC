c9becb1611efe44a510b97a226cad4a9d2d156e1
Akash OneNDF <akash@dbspl.co.in>
2024-07-10 21:10:43 +0530

    setup change

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  
-
-  return (
-    <>
-      
-    </>
-  )
+  return <div className="text-blue-400">hello</div>;
 }
 
-export default App
+export default App;
